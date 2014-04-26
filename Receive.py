88faba0a31ad2138750f42dbdb0f21bce33e4aab
dmi3 <dima@dmi3.net>
2014-04-26 19:56:24 +0300

    Introduced dates

--- Receive.py
+++ Receive.py
@@ -3,8 +3,30 @@ import os
 import sys
 import logging
 import traceback
+import datetime
 import re
 from TeuxDeux import TeuxDeux
+
+def extractDateNText(text):  
+  day = datetime.date.today().day
+  month = datetime.date.today().month
+  year = datetime.date.today().year
+
+  date_n_text=re.findall(r'^([1-3]?\d{1})/(1?\d?)/?(\d{0,4})\s?(.*)',text)
+  if len(date_n_text)>0 and len(date_n_text[0])==4:
+    
+    if date_n_text[0][0]!='':
+      day = int(date_n_text[0][0])
+
+    if date_n_text[0][1]!='':
+      month = int(date_n_text[0][1])
+
+    if date_n_text[0][2]!='':
+      year = int(date_n_text[0][2])
+
+    text = date_n_text[0][3]
+
+  return text, datetime.date(year, month, day)
 
 def main():
   text = ''
@@ -15,7 +37,7 @@ def main():
   logging.debug('Number %s have sent text: %s' % (os.environ['SMS_1_NUMBER'], text))
 
   td=TeuxDeux("user","pass")
-  td.create(text)
+  td.create(*extractDateNText(text))
 
 def log_except_hook(*exc_info):
   logging.exception("".join(traceback.format_exception(*exc_info)))
--- Test.py
+++ Test.py
@@ -3,29 +3,70 @@
 # sudo pip install mockito
 
 import unittest
+import datetime
 import os
 from mockito import when, any, verify
 
 from Receive import main
+from Receive import extractDateNText
 from TeuxDeux import TeuxDeux
 
-def do_nothing(s, n, p):
+def do_nothing(*p):
   pass
+
+class MockDate(datetime.date):
+  @classmethod
+  def today(self):
+    return self(2012, 5, 24)
 
 class Tests(unittest.TestCase):
   
   def setUp(self):
+    TeuxDeux.__init__ = do_nothing
+    when(TeuxDeux).create(any()).thenReturn("")
+    datetime.date = MockDate
+  
+  def testConcat(self):
     os.environ = {
       'SMS_1_NUMBER':'02',
       'SMS_0_TEXT':'Yello',
       'SMS_1_TEXT':' man!'}
-    TeuxDeux.__init__ = do_nothing
-    when(TeuxDeux).create(any()).thenReturn("")
-  
-  def testOne(self):
+
     main()
 
-    verify(TeuxDeux).create("Yello man!")
+    verify(TeuxDeux).create("Yello man!", MockDate.today())
+
+  def testDate(self):
+    os.environ = {
+      'SMS_1_NUMBER':'02',
+      'SMS_0_TEXT':'1/2 do stuff'}
+
+    main()
+
+    verify(TeuxDeux).create("do stuff", MockDate(2012, 2, 1))
+
+  def testExtractDateNText(self):
+    year = MockDate.today().year
+    month = MockDate.today().month
+
+    self.assertEqual(extractDateNText('28 text'), ('28 text', MockDate.today()) )
+
+    self.assertEqual(extractDateNText('28/ text'), ('text', MockDate(year, month, 28)))
+    self.assertEqual(extractDateNText('28/text'),  ('text', MockDate(year, month, 28)))
+    self.assertEqual(extractDateNText('8/ text'),  ('text', MockDate(year, month, 8)))
+    self.assertEqual(extractDateNText('8/'), ('', MockDate(year, month, 8)) )
+
+    self.assertEqual(extractDateNText('9/5 text'), ('text', MockDate(year, 5, 9)))
+    
+    self.assertEqual(extractDateNText('28/3 text'), ('text', MockDate(year, 3, 28)) )
+    self.assertEqual(extractDateNText('28/3text'), ('text', MockDate(year, 3, 28)) )
+    self.assertEqual(extractDateNText('8/3 text'), ('text', MockDate(year, 3, 8)) )
+    self.assertEqual(extractDateNText('8/3'), ('', MockDate(year, 3, 8)) )
+
+    self.assertEqual(extractDateNText('28/3/2018 text'), ('text', MockDate(2018, 3, 28)))
+    self.assertEqual(extractDateNText('28/3/2018text'), ('text', MockDate(2018, 3, 28)))
+    self.assertEqual(extractDateNText('8/3/2018 text'), ('text', MockDate(2018, 3, 8)))
+    self.assertEqual(extractDateNText('8/3/2018'), ('',MockDate(2018, 3, 8)) )
 
 
 if __name__ == '__main__':
