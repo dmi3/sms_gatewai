9e202c6e73da67b2a95d3a4acc2a8479079c07df
dmi3 <dima@dmi3.net>
2014-05-01 13:18:22 +0300

    cleanup

--- Test.py
+++ Test.py
@@ -12,7 +12,6 @@ from Receive import main
 from Receive import extractDateNText
 from TeuxDeux import TeuxDeux
 
-
 year = 2012
 month = 5
 day = 24
@@ -85,27 +84,27 @@ class DateTest(unittest.TestCase):
 
   def testDatesWithoutText(self):
     self.assertEqual(extractDateNText('29/5'), ('29/5', MockDate(year, 5, 29)))
-    self.assertEqual(extractDateNText('8/3/2018   '), ('8/3/2018   ',MockDate(2018, 3, 8)) )
+    self.assertEqual(extractDateNText('8/3/2018   '), ('8/3/2018   ',MockDate(2018, 3, 8)))
 
   def testDatesInPast(self):
     self.assertEqual(extractDateNText('28/%s/%s text' % (month-1,year)), ('28/%s/%s text' % (month-1,year), MockDate.today()))
     self.assertEqual(extractDateNText('8/ text'),  ('8/ text', MockDate.today()))
-    self.assertEqual(extractDateNText('8/'), ('8/', MockDate.today() ))
-    self.assertEqual(extractDateNText('8/5 text'), ('8/5 text', MockDate.today()) )
+    self.assertEqual(extractDateNText('8/'), ('8/', MockDate.today()))
+    self.assertEqual(extractDateNText('8/5 text'), ('8/5 text', MockDate.today()))
     self.assertEqual(extractDateNText('8/5'), ('8/5', MockDate.today()))
 
   def testNotDates(self):
-    self.assertEqual(extractDateNText(' 28.8.2018 text'), (' 28.8.2018 text', MockDate.today()) )
-    self.assertEqual(extractDateNText('28 text'), ('28 text', MockDate.today()) )
-    self.assertEqual(extractDateNText('32/13/2015 imposibiru'), ('32/13/2015 imposibiru', MockDate.today()) )
+    self.assertEqual(extractDateNText(' 28.8.2018 text'), (' 28.8.2018 text', MockDate.today()))
+    self.assertEqual(extractDateNText('28 text'), ('28 text', MockDate.today()))
+    self.assertEqual(extractDateNText('32/13/2015 imposibiru'), ('32/13/2015 imposibiru', MockDate.today()))
 
   def testSlashDelimeter(self):
     self.assertEqual(extractDateNText('28/ text'), ('text', MockDate(year, month, 28)))
     self.assertEqual(extractDateNText('28/text'),  ('text', MockDate(year, month, 28)))
 
     self.assertEqual(extractDateNText('31/5/ text'), ('text', MockDate(year, 5, 31)))  
-    self.assertEqual(extractDateNText('28/5 text'), ('text', MockDate(year, 5, 28)) )
-    self.assertEqual(extractDateNText('28/5text'), ('text', MockDate(year, 5, 28)) )
+    self.assertEqual(extractDateNText('28/5 text'), ('text', MockDate(year, 5, 28)))
+    self.assertEqual(extractDateNText('28/5text'), ('text', MockDate(year, 5, 28)))
 
     self.assertEqual(extractDateNText('28/3/2018 text'), ('text', MockDate(2018, 3, 28)))
     self.assertEqual(extractDateNText('28/3/2018text'), ('text', MockDate(2018, 3, 28)))
